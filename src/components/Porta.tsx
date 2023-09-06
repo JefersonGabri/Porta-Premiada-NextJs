a868e383d0025b933b98fd84d38e775783b3352a
JefersonSg <jefersongabri@gmail.com>
2023-09-06 19:43:33 -0300

    eu primeiro commit

--- src/components/Porta.tsx
+++ src/components/Porta.tsx
@@ -1,18 +1,15 @@
 import styles from '../styles/Porta.module.css'
 import React from 'react'
 
-interface Infos {
-  selecionada : boolean
-}
 
-const Porta = (props: Infos) => {
+const Porta = () => {
   const [abriu, setAbriu] = React.useState(false)
-   const selecionada = props.selecionada ? styles.selecionada : ''
+
   return (
     <div className={styles.area} >
-      <div className={`${styles.batente} ${selecionada ? styles.selecionada : ''} `}>
+      <div className={`${styles.batente}  `}>
         <div className={`${styles.porta} ${abriu ? styles.aberta : ''}`}>
-          <div className={`${styles.macaneta} ${selecionada ? styles.selecionada : ''}`} onClick={()=>{
+          <div className={`${styles.macaneta} `} onClick={()=>{
       setAbriu(true)
     }}></div>
           <div className={styles.numero}>3</div>
