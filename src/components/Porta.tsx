f044899bf53eeb494cb1cb4e88ab793b5a747457
JefersonSg <jefersongabri@gmail.com>
2023-09-07 14:50:27 -0300

    Adição de sombras no presente, perspectiva diferente no chão e background na porta premiada

--- src/components/Porta.tsx
+++ src/components/Porta.tsx
@@ -1,18 +1,31 @@
 import styles from '../styles/Porta.module.css'
 import React from 'react'
+import PortaModel from '@/models/porta'
+
+interface PortaProps {
+  value: PortaModel
+  onChange: (novaPorta: PortaModel) => void
+}
 
 
-const Porta = () => {
-  const [abriu, setAbriu] = React.useState(false)
+const Porta = (props: PortaProps) => {
+  const porta = props.value
+  const selecionada = porta.selecionada ? styles.selecionada : ''
+  // const [abrir, setAbri] = React.useState(false)
+
+  const alternarSelecao = () => props.onChange(porta.alternarSelecao())
+  const abrir = e => {
+    e.stopPropagation()
+    props.onChange(porta.abrir())
+  }
+
 
   return (
-    <div className={styles.area} >
-      <div className={`${styles.batente}  `}>
-        <div className={`${styles.porta} ${abriu ? styles.aberta : ''}`}>
-          <div className={`${styles.macaneta} `} onClick={()=>{
-      setAbriu(true)
-    }}></div>
-          <div className={styles.numero}>3</div>
+    <div className={`${styles.area} `} onClick={alternarSelecao} >
+      <div className={`${styles.batente}  ${selecionada}`}>
+        <div className={`${styles.porta} ${porta.aberta ? styles.aberta : ''} `}>
+          <div className={`${styles.macaneta} `} onClick={abrir}></div>
+          <div className={styles.numero}>{porta.numero}</div>
         </div>
       </div>
         <div className={styles.chao}></div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,11 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import Presente from '@/components/Presente';
 import Porta from '@/components/Porta';
+import PortaModel from '@/models/porta';
+import {useState} from 'react'
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
   return (
     <>
       <Head>
@@ -15,10 +18,7 @@ export default function Home() {
       <main className={styles.main}>
         <Presente />
         <div className={styles.portas}>
-          <Porta />
-          <Porta />
-          <Porta />
-          <Porta /> 
+          <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
         </div>
       </main>
     </>
